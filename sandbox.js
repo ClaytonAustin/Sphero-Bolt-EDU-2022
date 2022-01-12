4708d139234a207c0431e364913d87681270d24d
ClaytonAustin <austinclaytonbuisness@gmail.com>
2022-01-12 08:25:30 -0800

    Initial Sandbox commitment

---
+++ sandbox.js
@@ -0,0 +1,27 @@
+/*Programmer:Clayton Austin
+Date:1/12/22
+Program:Sandbox program 
+*/
+
+async function startProgram() {
+	// Write code here
+	/*Square 
+	setHeading(270);
+	await speak('hello square!', true);
+	setMainLed({ r: 255, g: 0, b: 0});
+		for (var _i1 = 0; _i1 < 4; _i1++) {
+			//loop this logic 4 times to create a square 
+			await Sound.Game.Coin.play(true);
+			setMainLed({ r: 0, g: 255, b:0});
+			await roll ((getHeading() + 90), 50, 2);
+			setMainLed({ r: 255, g: 0, b: 0});
+			await delay(1);
+		}
+	await Sound.Game.WinClassic.Play(true);*/
+	
+	
+	
+}
+
+
+
