61994852342a4af9a698dd90fef03fafb4eda9c1
ClaytonAustin <austinclaytonbuisness@gmail.com>
2022-01-13 09:05:22 -0800

    Triangle success

--- sandbox.js
+++ sandbox.js
@@ -24,4 +24,44 @@ async function startProgram() {
 }
 
 
+Commit 1
+async function startProgram() {
+	// Write code here
+	/*Square 
+	setHeading(270);
+	await speak('hello square!', true);
+	setMainLed({ r: 255, g: 0, b: 0});
+		for (var _i1 = 0; _i1 < 4; _i1++) {
+			//loop this logic 4 times to create a square 
+			await Sound.Game.Coin.play(true);
+			setMainLed({ r: 0, g: 255, b:0});
+			await roll ((getHeading() + 90), 50, 2);
+			setMainLed({ r: 255, g: 0, b: 0});
+			await delay(1);
+		}
+	await Sound.Game.WinClassic.Play(true);*/
 
+
+
+commit 2
+
+async function startProgram() {
+	// Write code here
+	/*Square 
+	setHeading(270);
+	await speak('hello triangle!', true);
+	setMainLed({ r: 255, g: 0, b: 0});
+		for (var _i1 = 0; _i1 < 4; _i1++) {
+			//loop this logic 3 times to create a square 
+			await Sound.Game.Coin.play(true);
+			setMainLed({ r: 0, g: 255, b:0});
+			await roll ((getHeading() + 120), 50, 2);
+			setMainLed({ r: 255, g: 0, b: 0});
+			await delay(1);
+		}
+	await Sound.Game.WinClassic.Play(true);*/
+
+
+
+
+commit 3
