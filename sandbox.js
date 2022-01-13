dd606dc1aa5653c0d3a792a7691ca69254b1f6e5
ClaytonAustin <austinclaytonbuisness@gmail.com>
2022-01-13 09:17:05 -0800

    Light Animation success

--- sandbox.js
+++ sandbox.js
@@ -51,7 +51,7 @@ async function startProgram() {
 	setHeading(270);
 	await speak('hello triangle!', true);
 	setMainLed({ r: 255, g: 0, b: 0});
-		for (var _i1 = 0; _i1 < 4; _i1++) {
+		for (var _i1 = 0; _i1 < 3; _i1++) {
 			//loop this logic 3 times to create a square 
 			await Sound.Game.Coin.play(true);
 			setMainLed({ r: 0, g: 255, b:0});
@@ -65,3 +65,24 @@ async function startProgram() {
 
 
 commit 3
+
+
+
+registerMatrixAnimation({
+	frames: [[[1, 1, 6, 6, 6, 6, 1, 1], [1, 6, 6, 6, 6, 6, 6, 1], [6, 6, 1, 6, 6, 1, 6, 6], [6, 6, 6, 6, 6, 6, 6, 6], [6, 6, 6, 1, 1, 6, 6, 6], [6, 6, 6, 1, 1, 6, 6, 6], [1, 6, 6, 6, 6, 6, 6, 1], [1, 1, 6, 6, 6, 6, 1, 1]], [[6, 6, 6, 6, 6, 6, 6, 6], [6, 6, 1, 6, 6, 1, 6, 6], [6, 6, 1, 6, 6, 1, 6, 6], [6, 1, 1, 6, 6, 1, 1, 6], [6, 6, 6, 6, 6, 6, 6, 6], [6, 1, 1, 1, 1, 1, 1, 6], [1, 6, 1, 1, 1, 1, 6, 1], [1, 1, 6, 6, 6, 6, 1, 1]]],
+	palette: [{ r: 255, g: 255, b: 255 }, { r: 255, g: 0, b: 0 }, { r: 255, g: 0, b: 0 }, { r: 255, g: 16, b: 0 }, { r: 255, g: 128, b: 0 }, { r: 255, g: 191, b: 0 }, { r: 255, g: 255, b: 0 }, { r: 185, g: 246, b: 30 }, { r: 0, g: 255, b: 0 }, { r: 185, g: 255, b: 255 }, { r: 0, g: 255, b: 255 }, { r: 0, g: 0, b: 255 }, { r: 145, g: 0, b: 211 }, { r: 157, g: 48, b: 118 }, { r: 255, g: 0, b: 255 }, { r: 204, g: 27, b: 126 }],
+	fps: 6,
+	transition: MatrixAnimationTransition.None
+});
+
+
+
+
+
+
+
+
+
+
+
+
